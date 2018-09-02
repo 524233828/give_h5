93f2e248ff5ad18f246e4203d3be197b6776f778
JoseChan <chenyu01@linghit.com>
2018-09-02 21:05:15 +0800

    fix

--- src/Middleware/Dispatch.php
+++ src/Middleware/Dispatch.php
@@ -2,10 +2,14 @@
 namespace Middleware;
 
 use Constant\ErrorCode;
+use Constant\JWTKey;
 use Exception\BaseException;
-use FastD\Http\Response;
 use FastD\Middleware\DelegateInterface;
 use FastD\Middleware\Middleware;
+use Firebase\JWT\JWT;
+use Logic\UserLogic;
+use Model\UserModel;
+use Monolog\Logger;
 use Psr\Http\Message\ServerRequestInterface;
 use Service\ApiResponse;
 
@@ -14,15 +18,47 @@ class Dispatch extends Middleware
 
     public function handle(ServerRequestInterface $request, DelegateInterface $next)
     {
+
+        $token = $request->getHeaderLine('Authorization');
+        if ($token) {
+            // 只做拆分获取用户ID，不判断可用性
+            try {
+                $decoded = (array)JWT::decode($token, JWTKey::KEY, [JWTKey::ALG]);
+                $user_id = isset($decoded['aud']) ? (string)$decoded['aud'] : 0;
+                if (!empty($user_id)) {
+                    $user = UserModel::getUserInfo($user_id, ['id', 'phone']);
+                    if (!empty($user)) {
+                        UserLogic::$user = $user;
+                    }
+                }
+            } catch (\Exception $e) {
+                UserLogic::$user = null;
+            }
+        }
+
+        $content_type = $request->getHeaderLine("content_type");
+
+        if($content_type == "application/json"){
+            $body = (string)$request->getBody();
+            $params = json_decode($body, true);
+
+            if($params){
+                $request->withQueryParams($params);
+            }
+        }
+
         try {
-            $response = $next($request);
+            $response = $next->process($request);
         } catch (\Exception $e) {
+            $log = myLog("error",Logger::ERROR);
+
+            $log->addError($e->getTraceAsString());
             if ($e->getCode()!==0) {
                 $response = new ApiResponse(
                     $e->getMessage(),
                     $e->getCode(),
-                    null,
-                    Response::HTTP_OK
+                    ["file" => $e->getFile(),"line"=>$e->getLine()],
+                    ErrorCode::status($e->getCode())
                 );
             } else {
                 BaseException::SystemError();
