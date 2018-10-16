f7b46e16b58cdd35908193090974cf45d08779b1
JoseChan <chenyu01@linghit.com>
2018-10-16 09:55:05 +0800

    fix

--- src/Logic/NotifyLogic.php
+++ src/Logic/NotifyLogic.php
@@ -20,6 +20,7 @@ class NotifyLogic extends BaseLogic
     {
         $log = myLog("NotifyLogic_payNotify");
         $out_trade_no = $request->getParam("out_trade_no");
+        $log->addDebug("body:".$request->getBody()->getContents());
 
         $log->addDebug("out_trade_no:". $out_trade_no);
         $order = OrderModel::getOrderByOrderId($out_trade_no);
